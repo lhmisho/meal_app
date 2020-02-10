3fda354ab867e5414a7ace4a4e40f256130c44bb
lhmisho <lhmisho@gmail.com>
2020-02-11 00:02:42 +0600

    styling and theming

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -6,6 +6,7 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
+      padding: const EdgeInsets.all(25),
       gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -9,7 +9,10 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(15),
-      child: Text(title),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.title,
+      ),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
         gradient: LinearGradient(
