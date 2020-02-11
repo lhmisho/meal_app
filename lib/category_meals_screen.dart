4a5132a47c823e5b0889645377e150ed9a44d963
lhmisho <lhmisho@gmail.com>
2020-02-11 21:59:11 +0600

    selecting meal according to category

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
   @override
   Widget build(BuildContext context) {
-    final routesArg = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final routesArg =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routesArg['title'];
     final categoryId = routesArg['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle, style: null),
       ),
       body: Center(
-        child: Text(
-          'The receipes for the category',
+        child: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (ctx, index) {
+            return Text(categoryMeals[index].title);
+          },
         ),
       ),
     );
