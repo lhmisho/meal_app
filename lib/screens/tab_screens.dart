472ed787a72a2d1abd42eb0c7b2c69b19ed836ac
lhmisho <lhmisho@gmail.com>
2020-02-12 22:53:36 +0600

    bottom navigation bar

--- lib/screens/tab_screens.dart
+++ lib/screens/tab_screens.dart
@@ -8,34 +8,44 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {'screen': CategoriesScreen(), 'title': 'Categories'},
+    {'screen': FavoritesScreen(), 'title': 'Favorites'},
+  ];
+  int _selectedScreenIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      initialIndex: 0,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.favorite),
-                text: 'Favorites',
-              )
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedScreenIndex]['title']),
+      ),
+      body: _pages[_selectedScreenIndex]['screen'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedScreenIndex,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Category'),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.favorite),
+            title: Text('Favorites'),
+          ),
+        ],
       ),
     );
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
   }
 }
