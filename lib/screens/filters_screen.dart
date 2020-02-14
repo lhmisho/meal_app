468c5ab5cb85e6921084ada19549a0c5004f3ad0
lhmisho <lhmisho@gmail.com>
2020-02-14 07:09:57 +0600

    added switch button on filter screen

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filter-screen';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _isGlutenFree = false;
+  bool _isVagetarian = false;
+  bool _isVegan = false;
+  bool _isLactoseFree = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,8 +22,86 @@ class FiltersScreen extends StatelessWidget {
       ),
       drawer: MainDrawer(),
       body: Center(
-        child: Text('filter screen'),
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                'Adjust your meal selection',
+                style: Theme.of(context).textTheme.title,
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  _buildSwitchTile(
+                    'Glutin free',
+                    'Only glutin-free meals!',
+                    _isGlutenFree,
+                    (val) {
+                      setState(
+                        () {
+                          _isGlutenFree = val;
+                        },
+                      );
+                    },
+                  ),
+                  _buildSwitchTile(
+                    'Vegetarian',
+                    'Only vageterian meals!',
+                    _isVagetarian,
+                    (val) {
+                      setState(
+                        () {
+                          _isVagetarian = val;
+                        },
+                      );
+                    },
+                  ),
+                  _buildSwitchTile(
+                    'Lactose free',
+                    'Only lactose free meals!',
+                    _isLactoseFree,
+                    (val) {
+                      setState(
+                        () {
+                          _isLactoseFree = val;
+                        },
+                      );
+                    },
+                  ),
+                  _buildSwitchTile(
+                    'Vegetarian',
+                    'Only Vean meals!',
+                    _isVegan,
+                    (val) {
+                      setState(
+                        () {
+                          _isVegan = val;
+                        },
+                      );
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildSwitchTile(
+    String title,
+    String subTitle,
+    bool currentValue,
+    Function updateValue,
+  ) {
+    return SwitchListTile(
+      title: Text(title),
+      subtitle: Text('subTitle'),
+      value: currentValue,
+      onChanged: updateValue,
     );
   }
 }
